6402b5dc14ad735d59e4e01bacffde4b3e6a0605
Katie Byers <lobsterkatie@gmail.com>
2018-12-23 18:06:33 -0700

    CSS wrangling

--- static/main.js
+++ static/main.js
@@ -9,3 +9,17 @@ $("#pictures").nanogallery2({
     { src: "img3.jpg", srct: "img3.jpg", title: "Test 3" }
   ]
 });
+setTimeout(function() {
+  $("svg").each(function() {
+    var svg = $(this);
+    var text = svg.find("text");
+    var bbox = text.get(0).getBBox();
+
+    svg
+      .get(0)
+      .setAttribute(
+        "viewBox",
+        [bbox.x, bbox.y, bbox.width, bbox.height].join(" ")
+      );
+  });
+}, 100);
